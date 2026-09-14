d76635c56a92a406caa11a570f67d774ab95194f
Bruno Dante <bruno.dante.nogue@gmail.com>
2026-09-14 13:20:07 -0300

    as rotas de listar, criar, editar e excluir livros

--- Funcionalidade 2 CRUD de livros/livros_routes.py
+++ Funcionalidade 2 CRUD de livros/livros_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash 
 from flask_login import login_required
 
 from models import db, Livro
